beabbd1f0331b230d2254f7fb883205300e3ee8a
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2025-03-11 16:26:16 +0000

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <div className="flex h-screen items-center justify-center ">
-      Pagina Inicial
+      Inicial
       <ModeSwitcherTheme />
     </div>
   )
