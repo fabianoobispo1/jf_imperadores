47d4c762280fb9be3149affa5198f41a3bfaa047
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2025-03-12 12:52:06 +0000

    teste

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
 
 import { auth } from '@/auth/auth'
-/* import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
- */
+
 export const metadata: Metadata = {
   title: 'JF Imperadores',
   description: 'JF Imperadores',
@@ -15,12 +15,12 @@ export default async function DashboardLayout({ children }: { children: React.Re
 
   if (!session?.user) redirect('/')
   return (
-    /*   <SidebarProvider>
-      <AppSidebar /> */
-    <main className="w-full">
-      {/*       <SidebarTrigger title="Menu" /> */}
-      {children}
-    </main>
-    /*     </SidebarProvider> */
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        {/*       <SidebarTrigger title="Menu" /> */}
+        {children}
+      </main>
+    </SidebarProvider>
   )
 }
