fd77ff2950879d5c6d46d253e32d1d737a36d0fe
fabianoobispo1 <fabianoobispo@icloud.com>
2025-12-08 15:00:44 -0300

    teste

--- src/app/(public_routes)/proposta_flag/page.tsx
+++ src/app/(public_routes)/proposta_flag/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div style={{ position: "fixed", inset: 0, background: "#000" }}>
       <iframe
-        src="/01.pdf"
+        src="/Proposta de Captação de Patrocínios JF Imperadores - Campeonato Mineiro 2026.pdf"
         title="Proposta PDF"
         style={{ width: "100%", height: "100%", border: "none" }}
       />
